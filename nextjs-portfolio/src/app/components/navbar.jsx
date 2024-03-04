0b6e4e45e70f56f765a70e9ff03e3b8959653335
bahm941 <baheer1234@live.com>
2024-03-04 20:25:38 +1300

    complete mobile landing page

--- nextjs-portfolio/src/app/components/navbar.jsx
+++ nextjs-portfolio/src/app/components/navbar.jsx
@@ -2,15 +2,12 @@ export default function Navbar() {
   return (
     <nav className="border-gray-200 bg-gray-800">
       <div className="flex flex-wrap items-center justify-between p-4">
-        <h1 className="pl-10 text-white font-inter text-3xl font-extrabold float-right">
+        <h1 className="pl-8 text-white font-inter text-xl font-extrabold">
           baheer.dev
         </h1>
-        <ul class="flex flex-col p-4 md:p-0 md:flex-row md:space-x-8 text-white">
+        <ul className="flex flex-col p-2 text-white pr-8 font-medium">
           <li className="sm:inline-block">
-            <a href="#">About Me</a>
-          </li>
-          <li className="sm:inline-block pr-10">
-            <a href="#">Contact</a>
+            <a href="#">Projects</a>
           </li>
         </ul>
       </div>
--- nextjs-portfolio/src/app/page.js
+++ nextjs-portfolio/src/app/page.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Navbar from "./components/navbar";
+import Image from 'next/image';
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -7,26 +9,40 @@ export default function Home() {
       <Head>
         <title>Baheer Ahmadzai</title>
       </Head>
-    <main>
-      <section className="bg-gray-800 flex flex-col w-screen h-screen font-inter">
-        <Navbar/>
-      <div>
-          <img
-            src="headshot-crop.jpg"
-            alt="me"
-            width={250}
-            height={250}
-            className="mr-4 rounded-full border-white border-4"
-          />
-            <h1 className="text-4xl font-extralight bg-gradient-to-r from-sky-600 to-sky-400 text-transparent bg-clip-text">
-              Baheer <span className="font-bold">Ahmadzai</span>
-            </h1>
-            <h3 className="text-l text-white ml-1">
-              Degree in <span className="font-bold">Computer Science</span> at
-              The University of Auckland
-            </h3>
-      </div>
+      <main>
+        <section className="bg-gray-800 min-w-screen min-h-screen font-inter">
+          <Navbar/>
+          <div className="text-center p-12 leading-10">
+          <div className=" py-4">
+              <h1 className="text-4xl font-extrabold font bg-gradient-to-r from-sky-600 to-sky-400 text-transparent bg-clip-text">Baheer Ahmadzai</h1>
+              <h3 className="text-l text-white ml-1">
+                Degree in <span className="font-bold">Computer Science</span> at
+                UoA
+              </h3>
+            </div>
+            <div className="flex justify-center">
+              <Image
+                src="/headshot-crop.jpg"
+                alt="me"
+                width={300}
+                height={300}
+                className="rounded-full border-white border-4 mt-2"
+              />
+            </div>
+            <div className="flex justify-center text-5xl p-5 gap-5">
+              <AiFillGithub/>
+              <AiFillLinkedin/>
+              <AiOutlineMail/>
+            </div>
+            <p className='leading-5 mt-2'>
+            I'm Baheer Ahmadzai! I am deeply intrigued by software development and automation tools. 
+            My passion lies in creating automation tools and crafting solutions that enhance efficiency. 
+            With a strong background in software and web development, 
+            I immerse myself in designing innovative applications and systems that streamline processes and solve everyday challenges.
+            </p>
+          </div>
       </section>
+
     </main>
     </div>
   );
