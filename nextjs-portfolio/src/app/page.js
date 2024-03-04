4e73cfee5096bdee291e81ffa2880c18d7c6c316
bahm941 <baheer1234@live.com>
2024-03-04 16:06:18 +1300

    add head shot image

--- nextjs-portfolio/src/app/page.js
+++ nextjs-portfolio/src/app/page.js
@@ -10,6 +10,22 @@ export default function Home() {
     <main>
       <section className="bg-gray-800 flex flex-col w-screen h-screen font-inter">
         <Navbar/>
+      <div>
+          <img
+            src="headshot-crop.jpg"
+            alt="me"
+            width={250}
+            height={250}
+            className="mr-4 rounded-full border-white border-4"
+          />
+            <h1 className="text-4xl font-extralight bg-gradient-to-r from-sky-600 to-sky-400 text-transparent bg-clip-text">
+              Baheer <span className="font-bold">Ahmadzai</span>
+            </h1>
+            <h3 className="text-l text-white ml-1">
+              Degree in <span className="font-bold">Computer Science</span> at
+              The University of Auckland
+            </h3>
+      </div>
       </section>
     </main>
     </div>
