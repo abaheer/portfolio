fa0a9ef9d34a78adecfb4f8c35711f0936c825b4
bahm941 <baheer1234@live.com>
2024-03-05 23:52:16 +1300

    fix project links

--- nextjs-portfolio/src/app/components/project.jsx
+++ nextjs-portfolio/src/app/components/project.jsx
@@ -1,7 +1,7 @@
 export default function Project(props) {
   return (
     <div className="p-5 m-5 shadow-lg bg-sky-900 rounded-2xl center-text">
-      <a href="https://github.com/abaheer/yt-to-drive" target="_blank">
+      <a href={props.link} target="_blank">
         <h1 className="font-medium text-2xl text-center">{props.name}</h1>
         <h2 className="mt-3">{props.desc}</h2>
         <div className="flex justify-center text-2xl p-5 gap-5">
