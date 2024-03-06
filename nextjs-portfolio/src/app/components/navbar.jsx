defa38382ed9ab4271dd648ca9bd027ce2afca74
bahm941 <baheer1234@live.com>
2024-03-06 18:59:56 +1300

    Update navbar.jsx

--- nextjs-portfolio/src/app/components/navbar.jsx
+++ nextjs-portfolio/src/app/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="border-gray-200 bg-gray-800">
       <div className="flex flex-wrap items-center justify-between p-4">
         <h1 className="pl-8 text-white font-inter text-xl font-extrabold">
-          baheer.dev
+          abaheer.com
         </h1>
         <ul className="flex flex-col p-2 text-white pr-8 font-medium">
           <li className="sm:inline-block">
