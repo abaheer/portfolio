2391e4322e922a58d887ae55476a8be16cc2f1cc
bahm941 <baheer1234@live.com>
2024-03-12 23:24:26 +1300

    add hover effect for projects

--- nextjs-portfolio/src/app/components/project.jsx
+++ nextjs-portfolio/src/app/components/project.jsx
@@ -1,6 +1,6 @@
 export default function Project(props) {
   return (
-    <div className="w-[500px]  bg-slate-700 aspect-square p-10 m-5 shadow-lg rounded-2xl center-text ">
+    <div className="transition ease-in-out delay-200 hover:-translate-y-2 hover:scale-110 hover:bg-slate-950 duration-300 w-[500px]  bg-slate-700 aspect-square p-10 m-5 shadow-lg rounded-2xl center-text ">
       <a href={props.link} target="_blank">
         <h1 className="font-bold text-3xl text-center">{props.name}</h1>
         <div className="flex justify-center text-4xl p-5 gap-5">
